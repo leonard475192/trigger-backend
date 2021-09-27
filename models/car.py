81299792ef9bfc9c1e48dfe56359489a1aacb6ec
leonard475192 <leonard.t1028@gmail.com>
2021-09-27 16:56:50 +0900

    :recycle: ref:import

--- models/car.py
+++ models/car.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Float, Date
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 
 from .database import Base
--- models/parking.py
+++ models/parking.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Float, Date
+from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.orm import relationship
 
 from .database import Base
