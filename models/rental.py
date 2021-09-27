0d7b70c7b05e8ca31ca976d6a00dcd87a338f323
leonard475192 <leonard.t1028@gmail.com>
2021-09-27 17:11:49 +0900

    :recycle: ref: all singular

--- models/rental.py
+++ models/rental.py
@@ -5,16 +5,16 @@ from .database import Base
 
 
 class Rental(Base):
-    __tablename__ = "rentals"
+    __tablename__ = "rental"
 
     id = Column(Integer, index=True, primary_key=True, comment="ID")
-    car_id = Column(Integer, ForeignKey("cars.id"))
-    locker_id = Column(Integer, ForeignKey("lockers.id"))
+    car_id = Column(Integer, ForeignKey("car.id"))
+    locker_id = Column(Integer, ForeignKey("locker.id"))
 
     in_use_flag = Column(Boolean, index=False, comment="利用中判定")
     fee_yen = Column(Integer, index=True, comment="15分あたりの料金")
     available_begin = Column(Date, index=True, comment="貸し出し開始時間")
     available_end = Column(Date, index=True, comment="貸し出し終了時間")
 
-    car = relationship("Car", back_populates="rentals")
+    car = relationship("Car", back_populates="rental")
     locker = relationship("Locker")
